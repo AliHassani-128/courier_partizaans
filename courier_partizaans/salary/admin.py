a98d6eaad943a6e0e9b3b6294a8776b2250db1ec
AliHassani-128 <alihassani128@yahoo.com>
2022-01-06 20:19:22 +0330

    add salary models to admin

--- courier_partizaans/salary/admin.py
+++ courier_partizaans/salary/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from salary.models import TripIncome, IncreaseIncome, DecreaseIncome, DailyIncome, WeekIncome
+
+admin.site.register(TripIncome)
+admin.site.register(IncreaseIncome)
+admin.site.register(DecreaseIncome)
+admin.site.register(DailyIncome)
+admin.site.register(WeekIncome)
