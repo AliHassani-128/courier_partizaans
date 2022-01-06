98988249baa3420994904e34f1e226d9504948fd
AliHassani-128 <alihassani128@yahoo.com>
2022-01-06 20:11:09 +0330

    add courier model

--- courier_partizaans/courier/models.py
+++ courier_partizaans/courier/models.py
@@ -3,5 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Courier(models.Model):
-    pass
+    name = models.CharField(unique=True,max_length=128)
+
+    def __str__(self):
+        return self.name
 
